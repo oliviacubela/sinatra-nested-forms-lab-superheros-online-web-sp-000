65fe9b6f346bd80f42672084838f6fb5c793b798
Olivia Cubela <oliviacubela@gmail.com>
2020-10-05 16:41:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    erb :super_hero
+  end
 
+  post '/teams' do
+    @team = Team.new(params[:team][:name], params[:team][:motto])
+    @superhero1 = SuperHero.new(params[:team][:hero][0][:name], params[:team][:hero][0][:power], params[:team][:hero][0][:bio])
+    @superhero2 = SuperHero.new(params[:team][:hero][1][:name], params[:team][:hero][1][:power], params[:team][:hero][1][:bio])
+    @superhero3 = SuperHero.new(params[:team][:hero][2][:name], params[:team][:hero][2][:power], params[:team][:hero][2][:bio])
+    erb :team
+    
+  end
 end
---
+++ app/models/team.rb
@@ -0,0 +1,16 @@
+class Team
+
+  attr_accessor :name, :motto
+
+  @@teams = []
+
+  def initialize(args)
+    @name = args[:name]
+    @motto = args[:motto]
+    @@teams << self
+  end
+
+  def self.all
+    @@teams
+  end
+end
